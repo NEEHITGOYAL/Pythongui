11b0e6c47b75abfafa01999c8ad854efb2159885
neehit <goyalneehit@gmail.com>
2021-03-28 11:20:19 +0530

    added ui files

--- Attendance.py
+++ Attendance.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 import cv2
 import numpy as np
 import face_recognition
 import os
 import requests
 from datetime import datetime
+import keyboard
 
 # from PIL import ImageGrab
  
@@ -78,9 +80,11 @@ while True:
             y1,x2,y2,x1 = faceLoc
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(255,0,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0),2)
             #markAttendance(name)
  
     cv2.imshow('Webcam',img)
     cv2.waitKey(30)
+    if keyboard.is_pressed('q'):
+        break;
